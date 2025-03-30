65a75bceaa77f0cfd933fa8fa8d5ce9183fd9a33
pikaC92 <claudia.brouche@gmail.com>
2025-03-30 20:56:22 +0200

    Fix bootstrap

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
 import {createApp} from 'vue';
 
-// import scss
-import './assets/styles/index.scss';
 
 import App from './App.vue'
 
 //Font awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-import { faDownload, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import {createRouter, createWebHistory} from "vue-router";
+import {faDownload, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
 // Importation de BootstrapVueNext
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import {bootstrapPlugin, Components} from 'bootstrap-vue-next';
-
-import HomeView from "@/views/HomeView.vue";
-import AboutView from "@/views/AboutView.vue";
 import router from "@/router";
+
+// import scss
+import './assets/styles/index.scss';
+
 
 // Ajout des icônes à la bibliothèque
 library.add(faDownload, faMagnifyingGlass);
@@ -33,7 +31,7 @@ app.use(bootstrapPlugin)
 
 // Enregistrement global des composants de bootstrap-vue-next
 for (const [key, component] of Object.entries(Components)) {
-  app.component(key, component);
+    app.component(key, component);
 }
 
 // Enregistrement global du composant FontAwesomeIcon
